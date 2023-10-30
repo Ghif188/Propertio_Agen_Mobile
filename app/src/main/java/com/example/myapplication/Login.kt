439c926498255717a8eec4f1fbfccc281bb57aa5
Ghif188 <ghifarikahfi162@gmail.com>
2023-10-30 13:34:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        with(binding){
+            IntentToLogin = Intent()
+        }
+    }
+}

5d28041cab5d1d301040cbb54ec02efdb30af695
Ghif188 <ghifarikahfi162@gmail.com>
2023-11-16 17:38:30 +0700

    Perbaikan dan Input Video

---
+++ app/src/main/java/com/example/myapplication/InputApartemen.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputApartemen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_apartemen)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/InputTanah.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputTanah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_tanah)
+    }
+}

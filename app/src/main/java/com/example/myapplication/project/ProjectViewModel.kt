ba081bf2ff95bd66cb88c38110d553b6acf4685d
rizkyoktarinanto <rizkyoktarinanto@mail.ugm.ac.id>
2023-12-18 08:39:36 +0700

    bug fixing

--- app/src/main/java/com/example/myapplication/project/ProjectViewModel.kt
+++ app/src/main/java/com/example/myapplication/project/ProjectViewModel.kt
@@ -8,7 +8,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ProyekViewModel : ViewModel() {
+class ProjectViewModel : ViewModel() {
     val propertyListLiveData: MutableLiveData<List<Properti>> = MutableLiveData()
 
     fun getPropertyList(api: PropertyApi) {
